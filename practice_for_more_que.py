aabacc1abda52d2ed6b762ddc97369efc4cbf1da
JustLuckkyy <luckychoudhary701413@gmail.com>
2025-12-25 21:35:08 +0530

    practice questions of functions in python

---
+++ practice_for_more_que.py
@@ -0,0 +1,63 @@
+# data = [45, 0, 88, 120, 0, 30, 0]
+# clean_data = []
+# zero = 0
+# for i in data:
+#     if zero in data:
+#         clean_data.append(i)
+#         zero += 1
+#         continue
+#     print(clean_data)
+
+
+## Challenge: Data Cleaner (Removing Zeroes from a Dataset)
+#
+# data = [45, 0, 88, 120, 0, 30, 0]
+# clean_data = []
+#
+# for num in data:
+#     if num != 0:
+#         clean_data.append(num)
+#
+# print("Original Data:", data)
+# print("Cleaned Data: ", clean_data)
+
+
+# users = ["lucky", "admin", "tester01", "guest_user"]
+#
+# name = input("what is your name?'")
+#
+# for user in users:
+#     if name in users:
+#         print("access granted")
+#         break
+#     else:
+#         print("access denied")
+#         break
+
+
+# for i in range(5,-1,-1):
+#     for j in range(i,0,-1):
+#         print(j, end=' ')
+#
+#     print()
+
+# ## Challenge: Inverted Countdown Triangle
+#
+# # Outer loop starts at 5 and counts down to 1
+# for i in range(5, 0, -1):
+#
+#     # Inner loop starts at the current value of i
+#     # It counts down until it reaches 1 (so stop at 0)
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+#
+#     # Move to the next line
+#     print()
+
+
+
+
+
+
+
+
